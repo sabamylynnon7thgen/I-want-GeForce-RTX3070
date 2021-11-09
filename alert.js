456494af86e9c5692cd8dc0b9e2d1c8947981979
SR3QR <sabamylynn7640X@gmail.com>
2021-11-09 10:45:46 +0900

    master

---
+++ alert.js
@@ -0,0 +1,93 @@
+
+function a1(){
+    alert('主のTwitterのフォロワーの人数:1062\nフォロー中の人数:1046')
+}
+function a2(){
+    alert('チャンネル登録してクレメンス')
+}
+function a3(){
+    var hk=Math.floor(Math.random()*14)
+    if(hk==0){
+        alert('Core i7-4790K\n4C8T\n4.0GHz\n4.4GHz\n88w\nHaswell(DevilsCanyon製品群)\nXP2000さんのメイン機に使われたCPU.\n7700Kが出るまでは最もクロックが高いCPUだった')
+    }
+    if(hk==1){
+        alert('Ryzen 9 3900X\n12C24T\n3.8GHz\n4.6GHz\n105w\nZen2(Matisse製品群)\nかのG氏のメイン機やXP2000さんのPC-8801改PCに使われている\n280mm簡易水冷推奨とかいうバケモノ')
+    }
+    if(hk==2){
+        alert('Core i7-8565U\n4C8T\n1.8GHz\n4.6GHz\n15w\nWhiskeyLake-U\nチューニング次第では大きく化けるCPU\n8550Uからターボは大幅にクロックアップ')
+    }
+    if(hk==3){
+        alert('Corei5-9400F\n6C6T\n2.9GHz\n4.1GHz\n65w\nCoffeeLakeReflesh-S\nintelが狂ったような値段で売り出したintelらしくないiGPU無効のCPU\n性能はRyzen 5 3500と同等であるがゲーミングにおいては圧倒的なつよさを持つintelらしい一面も見える')
+    }
+    if(hk==4){
+        alert('Ryzen 5 3600\n6C12T\n3.6GHz\n4.2GHz\n65w\nZen2(Matisse製品群)\n2019年7月7日発売モデルの中では最廉価なモデルだが格上のCore i7-8700Kよりも性能が高い(R15 nT1500cbオーバー)ことや8700Kよりもエコであるので完全に8700Kにトドメを刺したCPUである\nコスパは2019年のCPUの中では最も高い')
+    }
+    if(hk==5){
+        alert('Core i5-10400\n6C12T\n2.9GHz\n4.3GHz\n65w\nCometLake-S\n中３くんが自作するときにつかったCPU\ni5にHTが有効化されている(全モデル有効は初)、のでさらに強くなった\ni7-8700よりも早い\nRyzenの存在って大きいね')
+    }
+    if(hk==6){
+        alert('Core i7-8700K\n6C12T\n3.70GHz\n4.70GHz\n95w\nCoffeeLake-S\nとうとうメインストリームのi7が6コアになりました(Sandy-Eなどエンスージアスト向けには既にある)\nコア数が増えたにもかかわらずTDPは7700Kと比べて4wしか増えていません！\nこのCPUの1個うえに8086Kがあり定格、最大ともに300MHz上昇している')
+    }
+    if(hk==7){
+        alert('Ryzen 7 3700X\n8C16T\n3.6GHz\n4.4GHz\n65w\nZen2(Matisse製品群)\nCore i9-9900Kと同等性能ながらCinebench R15 nTをぶん回している際の消費電力は147wとかなりエコである\nとてもコスパのいいCPUなのでツイ主も欲しいと思っています')
+    }
+    if(hk==8){
+        alert('Ryzen 7 4700U\n8C8T\n2.0GHz\n4.1GHz\n15w\nZen2(Renoir製品群)\nRyzen 7のモバイル版\nintel遅れてると思わせるようなコア数(8Cores)とZen2の高い電力効率で異次元の性能です')
+    }
+    if(hk==9){
+        alert('Core i7-2600K\n4C8T\n3.4GHz\n3.8GHz\n95w\nSandyBridge\nSandy Bridgeマイクロアーキテクチャとか言う10年前の製品です\nこいつはとんでもないほどOC耐性が高く5GHzは朝飯前のレベルです。\nなお発売当時はものすごい円高の影響でi7なのに2.9万台で買えたそうです')
+    }
+    if(hk==10){
+        alert('Core i3-7100U\n2C4T\n2.4GHz\nTBなし\n15w\nKabyLake-U\n次のノート用i3(8130U)にあっさりと負けてしまうCPU(8130U以降はTB有効)\nKabyLakeでRyzenにボロ負けしたからしかたないなぁ\niGPUの動作クロックは最大1GHz!!')
+    }
+    if(hk==11){
+        alert('Core i7-1165G7\n4C8T\n2.8GHz\n4.7GHz\nTDP flexible Range\nTigerLake-U\nintel反撃ののろしをあげたCPU\nintel独自の技術で生み出された10nmプロセスのi7のノート用に仕上がった感じ\n最大動作クロックが10nm i7では初めて4.5GHzを超えたのである')
+    }
+    if(hk==12){
+        alert('Ryzen 7 ExtremeEdition\n8C16T\n1.8GHz\n4.2GHz\n15w\nZen2(Renoir製品群)\nNECのLaVie N15の最上位モデルのみに搭載されたド変態\nベースになったCPUはRyzen 7 4800Uではないかとされる\n本当に15wの冷却装備で冷やしきれるのかが懸念材料となる。')
+    }
+    if(hk==13){
+        alert('Core 2 Duo E8500\n2C2T\n3.16GHz\n1333MHz FSB\n65w\nPenryn(Wolfdale)\nCore 2 Duoの中では上から2番目のクロックの持ち主\n中学生がよく使ってる印象がある\nOC耐性はE0ステッピングがかなり高く条件次第ではE8400の世界記録を超えることも可能である')
+    }
+}
+function a4(){
+    alert('Githubで遊ぼう')
+}
+function a5(){
+    const CPU=["Pentium DualCore E2220","Core 2 Duo E6850","Core 2 Duo E6300","Core 2 Extreme QX6700","Core 2 Extreme X6800","Core 2 Extreme QX6850","Pentium DualCore E6300","Pentium DualCore E6500","Core 2 Duo E7200","Core 2 Duo E7500","Core 2 Duo E8400","Core 2 Duo E8500(SLB9K)","QHEX(Core 2 Duo E8600のES)","Core 2 Duo E8600","Core 2 Duo E8600@3.34GHz","Core 2 Quad Q9450","Core 2 Quad Q9550","Core 2 Quad Q9650","Core 2 Extreme QX9770","Celeron G1101","Pentium G6950","Core i3-550","Core i5-650","Core i5-655K","Core i5-750","Core i7-860","Core i7-875K","Celeron G460","Celeron G530","Celeron G540","Pentium G640","Core i3-2100","Core i3-2125","Core i5-2400","Core i5-2500K","Core i5-2550K","Core i7-2600","Core i7-2600K","Core i7-2700K","Xeon E3-1270","Celeron G1610","Core i3-3240","Core i5-3340S","Core i5-3340","Core i5-3450","Core i5-3470","Core i5-3470S","Core i5-3550","Core i7-3770","Xeon E3-1225 v2","Xeon E3-1245v2","Xeon E3-1270 v2","Xeon E3-1280 v2","Xeon E3-1290 v2","Celeron G1820","Celeron G1820TE","Pentium G3240","Pentium G3258","Core i3-4130","Core i3-4160","Core i3-4340","Core i3-4350","Core i3-4370","Core i5-4440","Core i5-4460","Core i5-4570","Core i5-4590","Core i5-4670","Core i5-4670K","Core i5-4690K","Core i7-4770","Core i7-4770K","Core i7-4771","Core i7-4790","Core i7-4790K","Xeon E3-1225v3(SR14U)","Xeon E3-1270v3","Core i5-5675C","Core i7-5775C","Core i7-920","Core i7-930","Core i7-940","Core i7-950","Core i7-960","Core i7-965 EE","Xeon L5640","Core i7-970","Core i7-980","Core i7-980X","Core i7-990X","Xeon X5698","Core i3-6100","Core i5-6400T","Core i5-6500","Core i5-6600K","Core i7-6700K","Core i5-7400","Core i5-7500","Core i5-7600(SR334)","Core i7-7700","Core i7-7700K","Celeron G3900","Ryzen 3 1200","Ryzen 3 1300X","Ryzen 5 1400","Ryzen 5 1500X","Ryzen 5 1600","Ryzen 5 1600(8Core)","Ryzen 5 1600X","Ryzen 7 1700","Ryzen 7 1700X","Ryzen 7 1800X","Celeron G4950","Core i7-5820K","Core i7-5960X","Core i5-7640X","Core i7-7740X","Core i7-6900K","Core i7-6950X","Xeon E5-2660v2 x2","Xeon E5-2603v2 x2","Xeon E5-2650Lv2 x2","Xeon E5-2680v2 x2","Xeon E5-2696v2 x2","Core i3-8100","Core i3-8350K","Core i5-8400","Core i5-8600K","Core i7-8700","Core i7-8700K","Core i7-8086K","Xeon E-2176G","Core i3-9100F","Core i5-9400F","Core i5-9500","Core i5-9600K","Core i7-9700","Core i7-9700K(P0)","Core i7-9700K(R0)","QQBY(Core i9-9900K ES)","Core i9-9900","Core i9-9900K","Core i9-9900KS","Xeon E-2286G","Xeon E-2288G","Core i3-10100F","Core i3-10105F","Core i3-10320","Core i5-10400F","Core i5-10400","Core i5-10600KF","Core i7-10700F","Core i7-10700K","Core i7-10700K@5.2GHz","Core i9-10850K","Core i9-10900K","Core i5-11400","Core i5-11400F","Core i5-11600K","Core i7-11700K","Core i9-11900K","Ryzen TR1900X","Ryzen TR1920X","Ryzen TR1950X","Ryzen 5 2600","Ryzen 5 2600X","Ryzen 7 2700","Ryzen 7 2700X","Ryzen TR2920X","Ryzen TR2950X","Ryzen TR2970WX","Ryzen TR2990WX","Ryzen 3 3200G","Ryzen 3 3100","Ryzen 3 3300X","Ryzen 5 3400G","Ryzen 5 3500","Ryzen 5 3600","Ryzen 5 3600X","Ryzen 5 3600XT","Ryzen 7 3700X","Ryzen 7 3800X","Ryzen 7 3800XT","Ryzen 9 3900","Ryzen 9 3900X","Ryzen 9 3900XT","Ryzen 9 3950X","Ryzen 3 PRO 4350G","Ryzen 5 PRO 4650G","Ryzen 7 PRO 4750G","Ryzen 5 5600X","Ryzen 5 5600G","Ryzen 7 5700G","Ryzen 7 5800X","Ryzen 9 5900X","Ryzen 9 5950X","Core i7-7800X","Core i7-7820X","Core i9-7900X","Core i9-7920X","Core i9-7940X","Core i9-7960X","Core i9-7980XE","Core i7-9800X","Core i9-9820X","Core i9-9900X","Core i9-9920X","Core i9-9940X","Core i9-9960X","Core i9-9980XE","Core i9-9990XE","Core i9-10900X","Core i9-10920X","Core i9-10940X","Core i9-10980XE","Ryzen TR3960X","Ryzen TR3970X","Ryzen TR3990X","Xeon Gold 5218R","Xoen Gold 5220R","Xeon Gold 6248R","Xeon Gold 6254","Xeon Platinum 8180","Xeon Platinum 8368Q","EPYC 7763","Xeon Gold 5218R x2","Xeon Gold 5220R x2","Xeon Gold 6248R x2","Xeon Gold 6254 x2","Xeon Platinum 8180 x2","Xeon Platinum 8368Q x2","EPYC 7763 x2","EPYC 7H12"],
+    const GPU=["GeForce GTX1660Super","Radeon RX6700XT","GeForce RTX3070(GA104-302)","GeForce RTX2060Super","GeForce RTX3070","GeForce RTX2070","GeForce RTX3090 NVLink SLI","GeForce GT710","GeForce GTX1060(6GB)","Radeon RX5500XT","Radeon RX5600XT","GeForce GTX970","GeForce GTX1080Ti 3waySLI","GeForce RTX3060","GeForce GTX1050Ti","GeForce GTX1650(GDDR6-TU116版)","Radeon RX Vega 56","Radeon RX6800","Quadro GV100","RTX A6000","RTX A6000 2Way QuadroSync SLI","GeForce RTX3060Ti","RTX A4000 4way","GeForce RTX3060Ti LHR OC","RTX A4000","Radeon RX6600XT","NVIDIA TITAN X(Maxwell)","NVIDIA TITAN X(Pascal)","Radeon HD4350 LP","GeForce GTX1080","GeForce GTX1070","GeForce 9800GTX","Radeon RX Vega64","GeForce GTX590","GeForce RTX3080","GeForce PCX5850(?)","GeForce RTX2080Super","Radeon RX6900XT","Radeon R9-380X","例の危険物(Radeon R9-295X2)","Radeon HD7970 GHz Edition","GeForce GTX770","GeForce RTX2060(6GB)","GeForce RTX2060(12GB)","GeForce GTX960 (4GB)","GeForce GTX780Ti","Radeon RX570(8GB)","Radeon R7-430 LP","Quadro K2200","Quadro K620","Quadro P6000","NVIDIA TITAN V","NVIDIA TITAN V 2Way NVLink","GeForce RTX2070Super","GeForce RTX2070Super 2way NVLinkSLI","Radeon RX590","Radeon HD4850","例の危険物のCrossFire","Radeon R9-390X","GeForce GTX660Ti","GeForce GTX480 2waySLI","GeForce GTX460","Radeon Pro SSG","Radeon RX480(8GB)","Radeon RX6800XT","GeForce RTX3080Ti","Quadro RTX8000","NVIDIA T1000","Quadro P600","Radeon HD6870","Quadro 4000","NVIDIA NVS810","GeForce GTX760(1.5GB)","GeForce GTX760(2GB)","GeForce GTX760(3GB)","GeForce GTX760(4GB)","GeForce GTX670","GeForce GTX950","GeForce GTX1650Super","Radeon RX550","Radeon 520","Radeon RX560","GeForce GTX750","GeForce GTX750Ti","GeForce GTX745 LP(2GB)","GeForce GTX745 LP(4GB)","GeForce 8800GTX","GeForce GTX1660","GeForce GTX1660Ti","GeForce RTX3080 LHR","GeForce RTX3070Ti","GeForce GTX260","Radeon R7-250X","Radeon R9-280","Radeon R9-290","Radeon R9-290X","Radeon R9-370","Radeon R9-270","GeForce GTX550Ti","GeForce GTX570","GeForce RTX2080Ti","GeForce GTX980Ti","RTX A2000","RTX A5000","Quadro M4000","GeForce GTX480","GeForce GTX1060(3GB)","GeForce GTX1050(3GB)","GeForce GTX1050(2GB)","Quadro 2000","GeForce GTX470","GeForce GTX580","GeForce GTS250","GeForce GTS450","GeForce GT240","Radeon HD5770","Radeon HD7790","Radeon Ⅶ","Radeon R9 Fury","Radeon R9 Nano","Radeon R9 Fury X","NVIDIA TITAN RTX","NVIDIA TITAN Xp","GeForce RTX2080","Radeon RX460","Radeon RX470","Radeon R7-260","Radeon R7-265"],
+    const RAM=["2GB","4GB","8GB","12GB","16GB","24GB","32GB","40GB","48GB","64GB","96GB","128GB","192GB","256GB","384GB","512GB"],
+    const HDD=["160GB","250GB","250GB(使用時間10万時間超)","320GB","500GB","640GB","750GB","1TB","1.5TB","2TB","3TB","4TB","5TB(15mm 2.5インチHDD)","6TB","8TB","10TB","10TB(エンタープライズ向け)","12TB(NAS向け)","14TB(エンタープライズ向け)","16TB(エンタープライズ向け)","18TB(6TB4本でRAID5)","18TB(エンタープライズ向け)","20TB(エンタープライズ向け)"],
+    const SSD=["Not installed","SATA 60GB","SATA 120GB","SATA 240GB","SATA 250GB","SATA 256GB","NVMe 256GB","SATA 480GB","SATA 500GB","SATA 512GB","NVMe 500GB","NVMe 512GB","NVMe(PCIe4.0)512GB","SATA 1TB","NVMe 1TB","NVMe(PCIe4.0) 1TB","SATA 2TB","NVMe 2TB","SATA 4TB","NVMe 2TB×2(RAID0)"],
+
+    cpu0=Math.floor(Math.random()*CPU.length)
+    cpu1=CPU[cpu0]
+    gpu0=Math.floor(Math.random()*GPU.length)
+    gpu1=GPU[gpu0]   
+    if(cpu0<=26){
+        ram0=Math.floor(Math.random()*5)
+    }else if(cpu0>=27&&cpu0<=62){
+        ram0=Math.floor(Math.random()*6+1)
+    }else if(cpu0>=63&&cpu0<=74){
+        ram0=Math.floor(Math.random()*8+1)
+    }else if(cpu0>=75&&cpu0<=99){
+        ram0=Math.floor(Math.random()*9+1)
+    }else if(cpu0>=100&&cpu0<=179){
+        ram0=Math.floor(Math.random()*10+2)
+    }else if(cpu0>=180&&cpu0<=186){
+        ram0=Math.floor(Math.random()*11+3)
+    }else{
+        ram0=Math.floor(Math.random()*6+10)
+    }
+    ram1=RAM[ram0]
+    hdd0=Math.floor(Math.random()*HDD.length)
+    hdd1=HDD[hdd0]
+    ssd0=Math.floor(Math.random()*SSD.length)
+    ssd1=SSD[ssd0]
+    ssdc=Math.floor(Math.random()*2)
+    if(ssdc==0){
+    ssd2=Math.floor(Math.random()*SSD.length)
+    ssd3=SSD[ssd2]
+    }else ssd3=SSD[0]
+    alert(`ゲストさんのPCのスペックは...\nCPU:${cpu1}\nGPU:${gpu1}\nRAM:${ram1}\nHDD:${hdd1}\nSSD:${ssd1}+${ssd3}\nです`)
+    }
