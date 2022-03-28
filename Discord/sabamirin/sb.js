7ca4c0ed5db3b3056a32297a99866e4d3fc87a18
SR3QR <sabamylynn7640X@gmail.com>
2022-03-28 16:59:50 +0900

    mar

---
+++ Discord/sabamirin/sb.js
@@ -0,0 +1,3 @@
+function sbpp01(){
+    alert('Lenovo IdeaPad Flex550\nCPU:Ryzen 7 4700U with AMD Radeon Graphics\nGPU:AMD Radeon Graphics 7コア(VRAM=512MB)\nRAM:DDR4-3200 16GB\nSSD:Samsung PM991 512GB\nディスプレイ:1920x1080[14.0インチ]\n価格:73040円')
+}
