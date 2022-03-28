b0edf86d20a94b51dcd8dd2368484fb2770e7c5d
SR3QR <sabamylynn7640X@gmail.com>
2022-03-28 18:51:53 +0900

    結果にコミットする、ライ○ップ

--- Discord/sabamirin/sb.js
+++ Discord/sabamirin/sb.js
@@ -1,3 +1,9 @@
 function sbpp01(){
-    alert('Lenovo IdeaPad Flex550\nCPU:Ryzen 7 4700U with AMD Radeon Graphics\nGPU:AMD Radeon Graphics 7コア(VRAM=512MB)\nRAM:DDR4-3200 16GB\nSSD:Samsung PM991 512GB\nディスプレイ:1920x1080[14.0インチ]\n価格:73040円')
+    alert('Lenovo IdeaPad Flex550\nCPU:Ryzen 7 4700U with AMD Radeon Graphics(8C/8T,2.0~4.2GHz)\nGPU:AMD Radeon Graphics 7コア(VRAM=512MB)\nRAM:DDR4-3200 16GB\nSSD:Samsung PM991 512GB\nディスプレイ:1920x1080[14.0インチ]\n価格:73040円')
+}
+function sbpp02(){
+    alert('NEC Mate J タイプML(ML-H)\nCPU:intel Core i5-4570(4C/4T,3.2~3.6GHz)\nGPU:intel HD Graphics 4600(VRAM=128MB)\nRAM:DDR3-1600 8GB\nHDD:WD Green 3TB(WD30EZRX)\nディスプレイ:1920x1080[23.8インチ]\n価格:12650円')
+}
+function sbpp03(){
+    alert('ASUS Zenfone 6\nSoC:Qualcomm Snapdragon 855\nRAM:8GB\nストレージ:UFS2.1 256GB+Samsung EvoPlus 128GB\nディスプレイ:2340x1080[6.38インチ]\n価格:31800円')
 }
